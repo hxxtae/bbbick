b06e22f808418b2c66b13a7cd946dce497a49414
hxxtae <fkdlxmfkdl@gmail.com>
2024-04-26 16:34:32 +0900

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { createPortal } from 'react-dom';
-
-import { AppLayout } from './AppLayout'
-import { AppRouter } from './router/AppRouter'
-import { useAuthSync } from './hooks/auth/useAuthSync';
-import { Loading } from './components/ui/Loading';
+import { AppLayout } from './layout/AppLayout'
+import AppRouter from './router/AppRouter'
 
 export const App = () => {
-  const { isLoading } = useAuthSync();
-
   return (
     <AppLayout>
-      <>
-        {isLoading ? createPortal(<Loading />, document.body) : <AppRouter />}
-      </>
+      <AppRouter/>
     </AppLayout>
   )
 }
-
