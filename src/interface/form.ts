507c71b4653a5617279ae3dd6b01c81a4376b670
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-01 15:03:08 +0900

    docs: 상품 업데이트 타입 수정 및 property 수정

--- src/interface/form.ts
+++ src/interface/form.ts
@@ -32,8 +32,6 @@ export interface IProductForm {
   pageNumber: string | null;
   weight: string | null;
   bookSize: string | null;
-  createAt: string | null;
-  updateAt: string | null;
 }
 
 export type IFilePath = "images";
