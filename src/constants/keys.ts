b385506f24db5229c6dc6b532837fd3e81843c43
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-01 15:04:51 +0900

    feat: 데이터 페이지네이션 hook 생성

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -2,3 +2,12 @@ export const localStorageKeys = {
   theme: "bbbick_theme" as const,
   files: "bbbick_image" as const
 }
+
+export const queryKeys = {
+  product: {
+    all: ['/product'] as const,
+    basic: (page: number) => [...queryKeys.product.all, `${page}`] as const,
+    category: (category: string) => [...queryKeys.product.all, `/${category}`] as const,
+    categoryScroll: (category: string, limit?: number) => [...queryKeys.product.all, `/${category}`, `/${limit}`] as const,
+  },
+}
