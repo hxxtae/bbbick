704bbc0cf0c199b06806778bb5e934bb67b53dcb
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-08 16:04:36 +0900

    docs: add cart remove api

--- src/hooks/cart/useSetCart.ts
+++ src/hooks/cart/useSetCart.ts
@@ -1,17 +1,18 @@
 import { arrayUnion, doc, setDoc } from 'firebase/firestore';
 import { useMutation, useQueryClient } from 'react-query'
+import { FirebaseError } from 'firebase/app';
 
 import { useAuthStore } from '@/store/useAuthStore';
 import { ProductType } from '@/interface/products';
+import { queryKeys } from '@/constants/keys';
 import { CartType } from '@/interface/cart'
 import { db } from '@/service/firebaseApp';
-import { queryKeys } from '@/constants/keys';
 
 export const useSetCart = () => {
   const auth = useAuthStore((state) => state.auth);
   const queryClient = useQueryClient();
 
-  // DB API
+  // [DB API - Set Cart (add)]
   const setCartStore = async (bookData: ProductType) => {
     if (!auth?.uid) {
       alert("로그인 후 다시 시도해주세요.");
@@ -44,12 +45,37 @@ export const useSetCart = () => {
         authCart: arrayUnion(cartData)
       }, { merge: true }); // 기존 데이터 형식을 유지하며, 업데이트 진행
     } catch (error) {
-      console.error("[Error]: Add Cart DB Error: ", error)
+      if (error instanceof FirebaseError) {
+        console.error("[Error] Add Cart DB Error: " + error.message);
+      } else {
+        console.error("[Error] Add Cart DB Error: " + error)
+      }
     }
   }
 
-  // Middle
-  const { isLoading, mutate } = useMutation((data: ProductType) => setCartStore(data), {
+// [DB API - Set Cart (remove)]
+  const removeCartStore = async () => {
+    if (!auth?.uid) {
+      alert("로그인 후 다시 시도해주세요.");
+      return;
+    }
+
+    const authId = auth.uid;
+    try {
+      await setDoc(doc(db, "cart", authId), {
+        authCart: []
+      });
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        console.error("[Error] Remove Cart DB Error: " + error.message);
+      } else {
+        console.error("[Error] Remove Cart DB Error: " + error)
+      }
+    }
+  }
+
+  // [Middle - add]
+  const { isLoading: addCartLoading, mutate: mutateCartAdd } = useMutation((data: ProductType) => setCartStore(data), {
     mutationKey: queryKeys.cart.all,
     onSuccess: () => {
       // 장바구니 데이터 최신화 (캐시 데이터 최신화)
@@ -60,11 +86,29 @@ export const useSetCart = () => {
     }
   })
 
-  // Run
+  // [Middle - remove]
+  const { isLoading: removeCartLoading, mutate: mutateCartRemove } = useMutation(() => removeCartStore(), {
+    mutationKey: queryKeys.cart.all,
+    onSuccess: () => {
+      // 장바구니 데이터 최신화 (캐시 데이터 최신화)
+      queryClient.invalidateQueries(queryKeys.cart.all)
+    },
+    onError: (error: any) => {
+      console.error("[Error] Remove Cart Error: ", error)
+    }
+  })
+
+  // [Run - add]
   const onAddCart = (data: ProductType) => {
-    if (isLoading) return;
-    mutate(data);
+    if (addCartLoading) return;
+    mutateCartAdd(data);
   }
 
-  return { onAddCart }
+  // [Run - remove]
+  const onRemoveCart = () => {
+    if (removeCartLoading) return;
+    mutateCartRemove();
+  }
+
+  return { onAddCart, onRemoveCart }
 }
