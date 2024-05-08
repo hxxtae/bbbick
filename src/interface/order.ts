47ef90099b527334a336fd74638a071ffed2739b
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-08 15:59:20 +0900

    :docs update interface
    - fix: auth interface
    - add: order interface

---
+++ src/interface/order.ts
@@ -0,0 +1,21 @@
+import { CartType } from './cart';
+
+export interface AddressType {
+  addressName: string; // 배송지명
+  address: string; // 주소
+  addressDetail: string; // 상세주소
+}
+
+export interface IOrder {
+  orderId: string;
+  orderCarts: CartType[];
+  orderPrice: number;
+  orderAddress: AddressType | null;
+  orderDate: string | null;
+}
+
+export type StepType = {
+  no: string;
+  name: string;
+  desc: string;
+}
