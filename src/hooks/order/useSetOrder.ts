b2a4a13a73591d1f7964ab02e2b887b9ba6f2b4a
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-10 13:00:52 +0900

    feat: 상품 주문취소 hook 구현

--- src/hooks/order/useSetOrder.ts
+++ src/hooks/order/useSetOrder.ts
@@ -1,5 +1,5 @@
-import { doc, writeBatch } from 'firebase/firestore';
-import { useMutation } from 'react-query';
+import { doc, updateDoc, writeBatch } from 'firebase/firestore';
+import { useMutation, useQueryClient } from 'react-query';
 
 import { useAuthStore } from '@/store/useAuthStore';
 import { FirebaseError } from 'firebase/app';
@@ -9,6 +9,11 @@ import { db } from '@/service/firebaseApp';
 
 export const useSetOrder = () => {
   const auth = useAuthStore((state) => state.auth);
+  const queryClient = useQueryClient();
+
+  // -------------------
+  // 주문등록 API
+  // -------------------
   const setOrderStore = async (data: IOrder) => {
     if (!auth?.uid) {
       alert("로그인 후 다시 시도해주세요.");
@@ -26,6 +31,7 @@ export const useSetOrder = () => {
     const authId = auth.uid;
     const createOrders = data.orderCarts.map((cart) => ({
       ...cart,
+      orderId: data.orderId,
       orderAddress: data.orderAddress,
       orderPrice: data.orderPrice,
       orderDate: data.orderDate,
@@ -33,28 +39,70 @@ export const useSetOrder = () => {
     }) as IHistory) as IHistory[];
     
     try {
-
       const batch = writeBatch(db);
       createOrders.forEach((orderData, idx) => {
-        const orderId = "ORDER" + "_" + idx.toString() + "_" + Date.now().toString();
-        batch.set(doc(db, "order", authId, "authOrder", orderId), { orderId, ...orderData });
+        const orderId = "ORD"+"_"+idx.toString()+"_"+Date.now().toString();
+        batch.set(doc(db, "order", authId, "authOrder", orderId), { ...orderData, orderId });
       });
       await batch.commit();
     } catch (error) {
-      console.error("[Error]: Add Order DB Error: ", error)
+      console.error("[Error]: Add Order Error: ", error);
     }
   }
 
-  const { isLoading, mutate } = useMutation((data: IOrder) => setOrderStore(data), {
+  // -------------------
+  // 주문취소 API
+  // -------------------
+  const setCancleOrderState = async (orderId: string) => {
+    if (!auth?.uid) {
+      alert("로그인 후 다시 시도해주세요.");
+      return;
+    }
+
+    try {
+      const orderRef = doc(db, "order", auth.uid, "authOrder", orderId);
+      await updateDoc(orderRef, {
+        deliverState: false
+      });
+    } catch (error) {
+      console.error("[Error]: Cancel Order Error: ", error);
+    }
+  }
+
+  const { isLoading: addOrderLoading, mutate: onAddOrder } = useMutation((data: IOrder) => setOrderStore(data), {
     mutationKey: queryKeys.order.all,
+    onSuccess: () => {
+      queryClient.invalidateQueries(queryKeys.order.all);
+    },
     onError: (error) => {
       if (error instanceof FirebaseError) {
-        console.error(error.message);
+        console.error("[Error]: Add Order Error: ", error.message);
       } else {
-        console.error(error);
+        console.error("[Error]: Add Order Error: ", error);
       }
     }
   });
 
-  return { addOrderLoading: isLoading, onAddOrder: mutate }
+  const { isLoading: cancelOrderLoading, mutate } = useMutation((orderId: string) => setCancleOrderState(orderId), {
+    mutationKey: queryKeys.order.all,
+    onSuccess: () => {
+      queryClient.invalidateQueries(queryKeys.order.all);
+    },
+    onError: (error) => {
+      if (error instanceof FirebaseError) {
+        console.error("[Error]: Cancel Order Error: ", error.message);
+      } else {
+        console.error("[Error]: Cancel Order Error: ", error);
+      }
+    }
+  })
+
+  const onCancelOrder = (orderId: string) => {
+    if (cancelOrderLoading) return;
+    if (confirm("상품주문을 취소하시겠습니까?")) {
+      mutate(orderId);
+    }
+  }
+
+  return { addOrderLoading, onAddOrder, cancelOrderLoading, onCancelOrder }
 }
