43ff5a883771a181631de60f701565390b0bd798
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-14 00:28:59 +0900

    docs: 사용자 동기화 hook 수정

--- src/hooks/auth/useAuthSync.ts
+++ src/hooks/auth/useAuthSync.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import { app, db } from '@/service/firebaseApp';
-import { IAuth } from '@/interface/auth';
+import { AuthAddition, IAuth } from '@/interface/auth';
 import { useAuthStore } from '@/store/useAuthStore'
 import { doc, getDoc } from 'firebase/firestore';
 
@@ -16,12 +16,14 @@ export const useAuthSync = () => {
       if (user) {
         const getAuthStoreData = async () => {
           const dataSnap = await getDoc(doc(db, "auth", user?.uid)); // 사용자 추가 데이터 가져오기
+          const { authType, authAddress } = dataSnap.data() as AuthAddition;
           setAuth({
             uid: user.uid,
             email: user.email,
             nickname: user.displayName,
-            authType: dataSnap.data()?.authType,
-            profileImg: null
+            profileImg: user.photoURL || null,
+            authType: authType || null,
+            authAddress: authAddress || null,
           } as IAuth)
           setIsLoading(false)
         }
