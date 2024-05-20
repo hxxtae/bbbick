d8878bc953b623c527195531712fb7ddc0d935e6
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-21 01:06:23 +0900

    docs: 사용하지 않는 Liblary 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { RecoilRoot } from 'recoil'
 import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
+  <>
     <GlobalStyle />
     <App />
-  </RecoilRoot>
+  </>
 )
