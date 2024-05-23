7d4da3eb9ef4cff976da0e8d587002c10d13a735
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-24 01:13:37 +0900

    docs: vite 설정 항목 수정
    - 빌드 시 파일 Gzip, Brotli 으로 압축

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import compression from 'vite-plugin-compression';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: 'gzip' }), // Gzip 압축 활성화
+    compression({ algorithm: 'brotliCompress', ext: '.br' }) // Brotli 압축 활성화
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
